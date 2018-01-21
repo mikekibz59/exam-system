aac67599553cf4ce09f41363cb7eebd452efcc38
mikekibz59 <mikekibz59@gmail.com>
2018-01-21 18:22:04 +0300

    efected neccesary changes to the required users

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+before_action :authenticate_student!
   def index
     @student = Student.all
   end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,4 +1,7 @@
 class TeachersController < ApplicationController
+include Accesible
+before_action :authenticate_teacher!
+skip_before_action :check_user, only:[:destroy]
   def index
     @teachers = Teacher.all
   end
